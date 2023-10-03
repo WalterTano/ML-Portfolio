9fb83e64c29e9ab6fb5fd1b2306703a165a770fc
Walter Taño <tano.walter@hotmail.com>
2023-10-02 22:18:34 -0300

    init

---
+++ src/components/brand-logo.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+export default function GatsbyDatoLogo() {
+  
+  return (<StaticImage
+    src="../favicon.png"
+    width="64"
+  />);
+}
